c061b34b9b16f3c6e7d9c97de59634f315dbc9f5
Martina Vokáčová <martinavokacova@gmail.com>
2022-04-02 22:12:53 +0200

    za sebou se načítající otázky

--- kviz.js
+++ kviz.js
@@ -1,9 +1,13 @@
 function priNacteni() {
+    vytvorStrukturuOtazky(poleVotazek[0]);
+}
+
+function vytvorStrukturuOtazky(otazecka) {
   let kviz = document.querySelector(".kviz");
 
   let otazka = document.createElement("div");
   otazka.setAttribute("id", "otazka");
-  otazka.innerText = "Fooooooo";
+  otazka.innerText = otazecka.text;
 
   let obsah = document.createElement("div");
   obsah.classList.add("obsah");
@@ -18,17 +22,18 @@ function priNacteni() {
   obsah.appendChild(moznosti);
   let obrazek = document.createElement("img");
   obrazek.setAttribute("id", "obrazek");
-  obrazek.src = "obrazky/pivo.jpg";
+  obrazek.src = otazecka.ilustrace;
 
   let odpovedi = document.createElement("ul");
   odpovedi.setAttribute("id", "odpovedi");
   moznosti.appendChild(odpovedi);
 
-  odpovediNaOtazku.forEach(function necoOdpoved(odp, index)
+  otazecka.odpovedi.forEach(function necoOdpoved(odp, index)
   {
     let odpoved = document.createElement("li");
     odpoved.setAttribute("data-odpoved", index);
     odpoved.innerText = odp;
+    odpoved.onclick = function click() { vytvorStrukturuOtazky(poleVotazek[1]) };
     
     odpovedi.appendChild(odpoved);
   });
@@ -39,5 +44,15 @@ function priNacteni() {
 }
 
 
-
-const odpovediNaOtazku = ["od1", "od2", "od3"];
+let poleVotazek = [otazecka1 = {
+    text: 'palmicka vole?',
+    ilustrace: 'obrazky/pivo.jpg',
+    odpovedi: ["odp1", "odp2", "odp3"],
+    spravnaOdpoved : 1
+}, otazecka2 = {
+    text: 'palmicka2 vole?',
+    ilustrace: 'obrazky/pivo.jpg',
+    odpovedi: ["od", "p2", "op3"],
+    spravnaOdpoved : 0
+}
+]
